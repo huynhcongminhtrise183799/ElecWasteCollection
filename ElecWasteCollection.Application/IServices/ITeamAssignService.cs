16ee2134435e1273c74277e5b9ee4ff5cfd97e83
Nnkhanhh <nnkhanh2303@gmail.com>
2025-11-19 19:33:27 +0700

    group and assign v1

---
+++ ElecWasteCollection.Application/IServices/ITeamAssignService.cs
@@ -0,0 +1,9 @@
+﻿using ElecWasteCollection.Application.Model;
+
+namespace ElecWasteCollection.Application.IServices
+{
+    public interface ITeamAssignService
+    {
+        Task<AssignTeamResult> AssignPostsToTeamsAsync(AssignTeamRequest request);
+    }
+}
